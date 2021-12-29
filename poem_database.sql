b1118c6f568313b0e914a6da1dd3403106467d92
uncogeek <80108440+uncogeek@users.noreply.github.com>
2021-12-29 14:29:51 +0330

    Add files via upload

---
+++ poem_database.sql
@@ -0,0 +1,43 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Dec 29, 2021 at 02:29 PM
+-- Server version: 8.0.18
+-- PHP Version: 7.2.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `poem`
+--
+CREATE DATABASE IF NOT EXISTS `poem` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+USE `poem`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_poem`
+--
+
+DROP TABLE IF EXISTS `tbl_poem`;
+CREATE TABLE IF NOT EXISTS `tbl_poem` (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `poem` text CHARACTER SET utf8 COLLATE utf8_persian_ci,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
